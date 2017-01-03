2fa5396567bbab595644c6a541fd602f7fc644ec
tekreme73 <tekreme73@gmail.com>
2017-01-03 16:18:45 +0100

    [CLEAN] Clean up and format code

--- src/fr/usmb/isc/m2/ExpressionInvalide.java
+++ src/fr/usmb/isc/m2/ExpressionInvalide.java
@@ -4,16 +4,18 @@ package fr.usmb.isc.m2;
  * La classe <code>ExpressionInvalide</code> est levee lorsque l'expression a
  * evaluer n'est pas correcte du point de vue syntaxique.
  */
-public class ExpressionInvalide extends Exception {
+public class ExpressionInvalide extends Exception
+{
+	
+	private static final long serialVersionUID = -8385644355249806406L;
 
-	public ExpressionInvalide() {
+	public ExpressionInvalide () {}
+
+	public ExpressionInvalide ( Exception cause ) {
+		super( cause );
 	}
 
-	public ExpressionInvalide(Exception cause) {
-		super(cause);
-	}
-
-	public ExpressionInvalide(String message) {
-		super(message);
+	public ExpressionInvalide ( String message ) {
+		super( message );
 	}
 }
--- src/fr/usmb/isc/m2/Serveur.java
+++ src/fr/usmb/isc/m2/Serveur.java
@@ -8,39 +8,42 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-public class Serveur {
-
-	public static void main(String[] args) throws UnknownHostException, IOException, ExpressionInvalide{
-
-		
-		System.out.println("serveur ON");
+public class Serveur
+{
+	
+	public static void main ( String[] args ) throws UnknownHostException, IOException, ExpressionInvalide
+	{
+		System.out.println( "serveur ON" );
 		
 		// Ouverture du socket, avec le port 2011
-		ServerSocket ServerS = new ServerSocket(2011);
+		ServerSocket ServerS = new ServerSocket( 2011 );
 		// Accepte les clients
 		Socket S = ServerS.accept();
 
 		// Tant que la socket est ouverte
-		while(true){
+		while ( true )
+		{
 			
 			// Récupère les informations du buffer
-			InputStreamReader ISR = new InputStreamReader(S.getInputStream());
-			BufferedReader BR = new BufferedReader(ISR);
+			InputStreamReader ISR = new InputStreamReader( S.getInputStream() );
+			BufferedReader BR = new BufferedReader( ISR );
 			String donnee = BR.readLine();
-			System.out.println(donnee);
+			System.out.println( donnee );
 			
 			// Si donnee est null
-			if (donnee == null) { break; }
+			if ( donnee == null )
+			{
+				break;
+			}
 
 			// Renvoie les données dans le buffer
-			PrintWriter PW = new PrintWriter(S.getOutputStream());
-			PW.println("a reçu les données");
+			PrintWriter PW = new PrintWriter( S.getOutputStream() );
+			PW.println( "a reçu les données" );
 			PW.flush();
 			
 		}
-		
-		
-		System.out.println("serveur OFF");
+
+		System.out.println( "serveur OFF" );
+		ServerS.close();
 	}
-	
 }
