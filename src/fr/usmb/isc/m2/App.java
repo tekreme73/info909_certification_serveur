2385b0aab2c3cda2135a684f871c1c681b9a42e9
tekreme73 <tekreme73@gmail.com>
2017-01-03 15:00:31 +0100

    [ADD] Main app class

---
+++ src/fr/usmb/isc/m2/App.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package fr.usmb.isc.m2;
+
+/**
+ * Classe principale de l'application serveur (03/01/2017)
+ * 
+ * @author Abdou ABDELMOUMNI(abdou.abdelmoumni@etu.univ-smb.fr)
+ * @author Rémi REBILLARD (remi.rebillard@etu.univ-smb.fr)
+ * @author Fanny RIBARD (fanny.ribard@etu.univ-smb.fr)
+ * @author Loïc ROBERGEON (loic.robergeon@etu.univ-smb.fr)
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
