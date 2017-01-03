b298b7bba6b309cca33f66a5c3d33d432969c611
Fanny <fanny.ribard@etu.univ-savoie.fr>
2017-01-03 15:42:12 +0100

    Communiquer à l'aide du buffer

--- src/fr/usmb/isc/m2/Serveur.java
+++ src/fr/usmb/isc/m2/Serveur.java
@@ -6,6 +6,12 @@ package fr.usmb.isc.m2;
 import java.io.IOException;
 import java.net.ServerSocket;
 
+import generic.BufferedReader;
+import generic.InputStreamReader;
+import generic.MoteurCalculSimple;
+import generic.PrintWriter;
+import generic.String;
+
 /**
  * 
  * @author Abdou ABDELMOUMNI(abdou.abdelmoumni@etu.univ-smb.fr)
@@ -25,6 +31,22 @@ public class Serveur
 
 	public Serveur ( int port ) throws IOException {
 		this.server = new ServerSocket( port );
+		
+		// Tant que la socket est ouverte
+		while(true){
+			
+			// Récupère les informations du buffer
+			InputStreamReader ISR = new InputStreamReader(S.getInputStream());
+			BufferedReader BR = new BufferedReader(ISR);			
+			
+			// Renvoie les données dans le buffer
+			PrintWriter PW = new PrintWriter(S.getOutputStream());
+			PW.println(res);
+			PW.flush();
+			
+		}
+				
+				
 	}
 
 	/**
