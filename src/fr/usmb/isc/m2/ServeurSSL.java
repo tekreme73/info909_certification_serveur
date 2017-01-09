7269e759bb56c33edfaa17dc07c7af0c5b807bcd
Abdou Abdelmoumni <abdouabdelmoumni@gmail.com>
2017-01-09 11:54:10 +0100

    Jean Neymar

--- src/fr/usmb/isc/m2/ServeurSSL.java
+++ src/fr/usmb/isc/m2/ServeurSSL.java
@@ -1,25 +1,88 @@
 package fr.usmb.isc.m2;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.UnknownHostException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 public class ServeurSSL
 {
-	public static void main ( String[] args ) throws UnknownHostException, IOException
+	public static void main ( String[] args ) throws UnknownHostException, IOException, NoSuchAlgorithmException, KeyStoreException
 	{
 		System.out.println( "serveur ON" );
 		SSLServerSocketFactory factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
 		
+		
+		SSLContext ctx = SSLContext.getInstance("TLS");
+		
+
+		TrustManager tm = new X509TrustManager() {
+	    
+
+		@Override
+		public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+			// TODO Auto-generated method stub xx
+			
+		}
+
+		@Override
+		public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public X509Certificate[] getAcceptedIssuers() {
+			// TODO Auto-generated method stub xx
+			return null;
+		}
+	};
+	
+		//KeyStore ks = KeyStore.getInstance("JKS");
+		//FileInputStream in = new FileInputStream("<path to cacerts"");
+		//ks.load(in, "changeit".toCharArray);
+	    
+		
+		InputStream in = new DataInputStream(new FileInputStream(new File("server.crt")));
+		KeyStore ks = KeyStore.getInstance("JKS");
+		ks.load(null, null);
+		
+		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+		X509Certificate cert = (X509Certificate) cf.generateCertificate(in);
+		in.close();
+		
+		ks.setCertificateEntry("dts", cert);
+		
+		char[] newpass = "password".toCharArray();
+		String name = "mykeystore.ks";
+		FileOutputStream output = new FileOutputStream(name);
+		ks.store(output, newpass);
+		
+		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+		kmf.init(ks, "password".toCharArray());
+		
+		
 		// Ouverture du socket, avec le port spécifique
 		int port = 8080;
 		SSLServerSocket ServerS = (SSLServerSocket) factory.createServerSocket( port );
+		//ServerS.set
 		// Accepte les clients
 		SSLSocket S = (SSLSocket) ServerS.accept();
 		S.startHandshake();
