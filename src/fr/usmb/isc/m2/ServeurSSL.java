05c14048e67602deeda87b11765d7e25ac5bb829
Abdou Abdelmoumni <abdouabdelmoumni@gmail.com>
2017-01-09 09:21:34 +0100

    jdjhjd

--- src/fr/usmb/isc/m2/ServeurSSL.java
+++ src/fr/usmb/isc/m2/ServeurSSL.java
@@ -1,6 +1,9 @@
 package fr.usmb.isc.m2;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,6 +21,29 @@ public class ServeurSSL {
     	
     	System.out.println("Serveur accepté");
         Socket socket = ssocket.accept();
+        
+     // Tant que la socket est ouverte
+     		while ( true )
+     		{
+     			
+     			// Récupère les informations du buffer
+     			InputStreamReader ISR = new InputStreamReader( socket.getInputStream() );
+     			BufferedReader BR = new BufferedReader( ISR );
+     			String donnee = BR.readLine();
+     			System.out.println( donnee );
+     			
+     			// Si donnee est null
+     			if ( donnee == null )
+     			{
+     				break;
+     			}
+
+     			// Renvoie les données dans le buffer
+     			PrintWriter PW = new PrintWriter( socket.getOutputStream() );
+     			PW.println( "a reçu les données" );
+     			PW.flush();
+     			
+     		}
 
         InputStream in = socket.getInputStream();
         OutputStream out = socket.getOutputStream();
