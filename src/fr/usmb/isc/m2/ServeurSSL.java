1c147f297d88cd5a58ea9996b793d70def3a5fab
tekreme73 <tekreme73@gmail.com>
2017-01-09 09:41:33 +0100

    [FIX] Weird while loop

--- src/fr/usmb/isc/m2/ServeurSSL.java
+++ src/fr/usmb/isc/m2/ServeurSSL.java
@@ -1,63 +1,52 @@
 package fr.usmb.isc.m2;
+
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
-import java.net.Socket;
+import java.net.UnknownHostException;
 
-import javax.net.ServerSocketFactory;
-import javax.net.ssl.SSLServerSocket;
-import javax.net.ssl.SSLServerSocketFactory;
 import javax.net.ssl.SSLSocket;
 
-public class ServeurSSL {
-  public static void main(String[] argv) throws Exception {
-    int port = 8080;
-    
-    SSLServerSocketFactory factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-    SSLServerSocket serverSocket = (SSLServerSocket) factory.createServerSocket(port);
-    
-    while(true){
-    	
-    	System.out.println("Serveur accepté");
-    	SSLSocket socket = (SSLSocket) serverSocket.accept();
-        socket.startHandshake();
-        System.out.println(socket.getRemoteSocketAddress());
-        
-     // Tant que la socket est ouverte
-     		while ( true )
-     		{
-     			
-     			// Récupère les informations du buffer
-     			InputStreamReader ISR = new InputStreamReader( socket.getInputStream() );
-     			BufferedReader BR = new BufferedReader( ISR );
-     			String donnee = BR.readLine();
-     			System.out.println( donnee );
-     			
-     			// Si donnee est null
-     			if ( donnee == null )
-     			{
-     				break;
-     			}
-
-     			// Renvoie les données dans le buffer
-     			PrintWriter PW = new PrintWriter( socket.getOutputStream() );
-     			PW.println( "a reçu les données" );
-     			PW.flush();
-     			
-     		}
-
-        InputStream in = socket.getInputStream();
-        OutputStream out = socket.getOutputStream();
-
-        // Read from in and write to out...
-
-        in.close();
-        out.close();
-    }
-    
-    
-  }
+public class ServeurSSL
+{
+	public static void main ( String[] args ) throws UnknownHostException, IOException
+	{
+		System.out.println( "serveur ON" );
+		
+		// Ouverture du socket, avec le port spécifique
+		int port = 8080;
+		ServerSocket ServerS = new ServerSocket( port );
+		// Accepte les clients
+		SSLSocket S = (SSLSocket) ServerS.accept();
+		S.startHandshake();
+        System.out.println(S.getRemoteSocketAddress());
+		
+		// Tant que la socket est ouverte
+		while ( true )
+		{
+			
+			// Récupère les informations du buffer
+			InputStreamReader ISR = new InputStreamReader( S.getInputStream() );
+			BufferedReader BR = new BufferedReader( ISR );
+			String donnee = BR.readLine();
+			System.out.println( donnee );
+			
+			// Si donnee est null
+			if ( donnee == null )
+			{
+				break;
+			}
+			
+			// Renvoie les données dans le buffer
+			PrintWriter PW = new PrintWriter( S.getOutputStream() );
+			PW.println( "a reçu les données" );
+			PW.flush();
+			
+		}
+		
+		System.out.println( "serveur OFF" );
+		ServerS.close();
+	}
 }
