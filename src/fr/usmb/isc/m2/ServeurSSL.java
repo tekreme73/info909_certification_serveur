c772ac4358cc58d0b740db59dccd46a73da13df9
tekreme73 <tekreme73@gmail.com>
2017-01-09 09:29:44 +0100

    [FIX] Unused classes

--- src/fr/usmb/isc/m2/ServeurSSL.java
+++ src/fr/usmb/isc/m2/ServeurSSL.java
@@ -1,59 +1,54 @@
 package fr.usmb.isc.m2;
+
 import java.io.BufferedReader;
-import java.io.InputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
 
 import javax.net.ServerSocketFactory;
 import javax.net.ssl.SSLServerSocketFactory;
 
-public class ServeurSSL {
-  public static void main(String[] argv) throws Exception {
-    int port = 443;
-    
-    ServerSocketFactory ssocketFactory = SSLServerSocketFactory.getDefault();
-    ServerSocket ssocket = ssocketFactory.createServerSocket(port);
-    
-    while(true){
-    	
-    	System.out.println("Serveur accepté");
-        Socket socket = ssocket.accept();
-        
-     // Tant que la socket est ouverte
-     		while ( true )
-     		{
-     			
-     			// Récupère les informations du buffer
-     			InputStreamReader ISR = new InputStreamReader( socket.getInputStream() );
-     			BufferedReader BR = new BufferedReader( ISR );
-     			String donnee = BR.readLine();
-     			System.out.println( donnee );
-     			
-     			// Si donnee est null
-     			if ( donnee == null )
-     			{
-     				break;
-     			}
+public class ServeurSSL
+{
+	
+	public static void main ( String[] args ) throws UnknownHostException, IOException
+	{
+		System.out.println( "serveur ON" );
+		ServerSocketFactory ssocketFactory = SSLServerSocketFactory.getDefault();
 
-     			// Renvoie les données dans le buffer
-     			PrintWriter PW = new PrintWriter( socket.getOutputStream() );
-     			PW.println( "a reçu les données" );
-     			PW.flush();
-     			
-     		}
+		// Ouverture du socket, avec le port 443
+		int port = 443;
+		ServerSocket ServerS = ssocketFactory.createServerSocket( port );
+		// Accepte les clients
+		Socket S = ServerS.accept();
+		
+		// Tant que la socket est ouverte
+		while ( true )
+		{
+			
+			// Récupère les informations du buffer
+			InputStreamReader ISR = new InputStreamReader( S.getInputStream() );
+			BufferedReader BR = new BufferedReader( ISR );
+			String donnee = BR.readLine();
+			System.out.println( donnee );
 
-        InputStream in = socket.getInputStream();
-        OutputStream out = socket.getOutputStream();
+			// Si donnee est null
+			if ( donnee == null )
+			{
+				break;
+			}
+			
+			// Renvoie les données dans le buffer
+			PrintWriter PW = new PrintWriter( S.getOutputStream() );
+			PW.println( "a reçu les données" );
+			PW.flush();
 
-        // Read from in and write to out...
-
-        in.close();
-        out.close();
-    }
-    
-    
-  }
+		}
+		
+		System.out.println( "serveur OFF" );
+		ServerS.close();
+	}
 }
