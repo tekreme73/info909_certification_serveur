a424368c5d3094cd56b316eb376ed706cad1da25
Abdou Abdelmoumni <abdouabdelmoumni@gmail.com>
2017-01-10 14:25:59 +0100

    Serveur en mieu

--- src/fr/usmb/isc/m2/Serveur.java
+++ src/fr/usmb/isc/m2/Serveur.java
@@ -1,6 +1,9 @@
 package fr.usmb.isc.m2;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -12,13 +15,28 @@ public class Serveur {
     System.setProperty("javax.net.ssl.keyStorePassword", "azerty");
     SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
     ServerSocket ss = ssf.createServerSocket(8080);
+    
+    // debut
     while (true) {
       Socket s = ss.accept();
+      
+   // Récupère les informations du buffer
+   			InputStreamReader ISR = new InputStreamReader( s.getInputStream() );
+   			BufferedReader BR = new BufferedReader( ISR );
+   			String donnee = BR.readLine();
+   			System.out.println( donnee );
+   			
+   			// Si donnee est null
+   			if ( donnee == null )
+   			{
+   				break;
+   			}
+   			
       PrintStream out = new PrintStream(s.getOutputStream());
-      out.println("Hi");
+      out.println("a recu - "+ donnee);
       out.close();
       s.close();
     }
-
+//fin
   }
 }
