5005226953dba1d3e5cc1fa579ee9903e9ab219d
Abdou Abdelmoumni <abdouabdelmoumni@gmail.com>
2017-01-10 15:17:07 +0100

    fdksnfl

--- src/fr/usmb/isc/m2/Serveur.java
+++ src/fr/usmb/isc/m2/Serveur.java
@@ -7,14 +7,23 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 
 public class Serveur {
   public static void main(String args[]) throws Exception {
     System.setProperty("javax.net.ssl.keyStore", System.getProperty( "user.dir" ) + "/resources/security/clientkeystore");
     System.setProperty("javax.net.ssl.keyStorePassword", "azerty");
-    SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-    ServerSocket ss = ssf.createServerSocket(8080);
+    
+    
+    //SSLContext ctx = SSLContext.getInstance("TLS");
+	//SSLSocketFactory ssf = ctx.getSocketFactory();
+	//SSLSocket ss = (SSLSocket) ssf.createSocket( "127.0.0.1", 8080 );
+    
+   SSLServerSocketFactory ssf = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+   ServerSocket ss = ssf.createServerSocket(8080);
     
     // debut
     while (true) {
@@ -23,17 +32,17 @@ public class Serveur {
    // Récupère les informations du buffer
    			InputStreamReader ISR = new InputStreamReader( s.getInputStream() );
    			BufferedReader BR = new BufferedReader( ISR );
-   			String donnee = BR.readLine();
-   			System.out.println( donnee );
-   			
-   			// Si donnee est null
-   			if ( donnee == null )
-   			{
-   				break;
-   			}
+//   			String donnee = BR.readLine();
+//   			System.out.println( donnee );
+//   			
+//   			// Si donnee est null
+//   			if ( donnee == null )
+//   			{
+//   				break;
+//   			}
    			
       PrintStream out = new PrintStream(s.getOutputStream());
-      out.println("a recu - "+ donnee);
+      //out.println("a recu - "+ donnee);
       out.close();
       s.close();
     }
--- src/fr/usmb/isc/m2/ServeurSSL.java
+++ src/fr/usmb/isc/m2/ServeurSSL.java
@@ -33,78 +33,78 @@ public class ServeurSSL
 {
 	public static void main ( String[] args ) throws UnknownHostException, IOException, NoSuchAlgorithmException, KeyStoreException, CertificateException, UnrecoverableKeyException
 	{
-		System.out.println( "serveur ON" );
-		SSLServerSocketFactory factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-		
-		
-		SSLContext ctx = SSLContext.getInstance("TLS");
-				
-		InputStream in = new DataInputStream(new FileInputStream(new File(System.getProperty( "user.dir" ) + "/resources/security/client.cer")));
-		KeyStore ks = KeyStore.getInstance("JKS");
-		ks.load(null, null);
-		
-		CertificateFactory cf = CertificateFactory.getInstance("X.509");
-		X509Certificate cert = (X509Certificate) cf.generateCertificate(in);
-		
-		in.close();
-		
-		ks.setCertificateEntry("dts", cert);
-		
-		char[] newpass = "azerty".toCharArray();
-		//String name = "mykeystore.ks";
-		//FileOutputStream output = new FileOutputStream(name);
-		//ks.store(output, newpass);
-		
-		/* créer un SSLContext utilisant cette TrustManagerFactory */
-		/* ici on configure le contexte SSL */
-		
-		TrustManagerFactory tmf = TrustManagerFactory.getInstance( "X.509" );
-		tmf.init( new CertPathTrustManagerParameters( params ) );
-		
-		ctx.init( null, tmf.getTrustManagers(), null );
-		
-		SSLSocketFactory sslSocketFactory = ctx.getSocketFactory();
-		
-		SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket( address, port );
-
-		
-		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-		kmf.init(ks, "password".toCharArray());
-		
-		
-		// Ouverture du socket, avec le port spécifique
-		int port = 8080;
-		SSLServerSocket ServerS = (SSLServerSocket) factory.createServerSocket( port );
-		//ServerS.set
-		// Accepte les clients
-		SSLSocket S = (SSLSocket) ServerS.accept();
-		S.startHandshake();
-        System.out.println(S.getRemoteSocketAddress());
-		
-		// Tant que la socket est ouverte
-		while ( true )
-		{
-			
-			// Récupère les informations du buffer
-			InputStreamReader ISR = new InputStreamReader( S.getInputStream() );
-			BufferedReader BR = new BufferedReader( ISR );
-			String donnee = BR.readLine();
-			System.out.println( donnee );
-			
-			// Si donnee est null
-			if ( donnee == null )
-			{
-				break;
-			}
-			
-			// Renvoie les données dans le buffer
-			PrintWriter PW = new PrintWriter( S.getOutputStream() );
-			PW.println( "a reçu les données" );
-			PW.flush();
-			
-		}
-		
-		System.out.println( "serveur OFF" );
-		ServerS.close();
+//		System.out.println( "serveur ON" );
+//		SSLServerSocketFactory factory = (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
+//		
+//		
+//		SSLContext ctx = SSLContext.getInstance("TLS");
+//				
+//		InputStream in = new DataInputStream(new FileInputStream(new File(System.getProperty( "user.dir" ) + "/resources/security/client.cer")));
+//		KeyStore ks = KeyStore.getInstance("JKS");
+//		ks.load(null, null);
+//		
+//		CertificateFactory cf = CertificateFactory.getInstance("X.509");
+//		X509Certificate cert = (X509Certificate) cf.generateCertificate(in);
+//		
+//		in.close();
+//		
+//		ks.setCertificateEntry("dts", cert);
+//		
+//		char[] newpass = "azerty".toCharArray();
+//		//String name = "mykeystore.ks";
+//		//FileOutputStream output = new FileOutputStream(name);
+//		//ks.store(output, newpass);
+//		
+//		/* créer un SSLContext utilisant cette TrustManagerFactory */
+//		/* ici on configure le contexte SSL */
+//		
+//		TrustManagerFactory tmf = TrustManagerFactory.getInstance( "X.509" );
+//		tmf.init( new CertPathTrustManagerParameters( params ) );
+//		
+//		ctx.init( null, tmf.getTrustManagers(), null );
+//		
+//		SSLSocketFactory sslSocketFactory = ctx.getSocketFactory();
+//		
+//		SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket( address, port );
+//
+//		
+//		KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+//		kmf.init(ks, "password".toCharArray());
+//		
+//		
+//		// Ouverture du socket, avec le port spécifique
+//		int port = 8080;
+//		SSLServerSocket ServerS = (SSLServerSocket) factory.createServerSocket( port );
+//		//ServerS.set
+//		// Accepte les clients
+//		SSLSocket S = (SSLSocket) ServerS.accept();
+//		S.startHandshake();
+//        System.out.println(S.getRemoteSocketAddress());
+//		
+//		// Tant que la socket est ouverte
+//		while ( true )
+//		{
+//			
+//			// Récupère les informations du buffer
+//			InputStreamReader ISR = new InputStreamReader( S.getInputStream() );
+//			BufferedReader BR = new BufferedReader( ISR );
+//			String donnee = BR.readLine();
+//			System.out.println( donnee );
+//			
+//			// Si donnee est null
+//			if ( donnee == null )
+//			{
+//				break;
+//			}
+//			
+//			// Renvoie les données dans le buffer
+//			PrintWriter PW = new PrintWriter( S.getOutputStream() );
+//			PW.println( "a reçu les données" );
+//			PW.flush();
+//			
+//		}
+//		
+//		System.out.println( "serveur OFF" );
+//		ServerS.close();
 	}
 }
